83e0c6e6674514afb6e8fbb606e1930b70ec555e
AaltonenJari <jaritapani.aaltonen@gmail.com>
2025-12-12 14:27:25 +0200

    Tietokantamäärittelyjen eriyttäminen moduuliksi

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(express.json())
@@ -14,30 +17,6 @@ const requestLogger = (request, response, next) => {
 }
 app.use(requestLogger)
 
-const mongoose = require('mongoose')
-
-
-// ÄLÄ KOSKAAN TALLETA SALASANOJA GitHubiin!
-const password = process.argv[2]
-const url = `mongodb+srv://jaritapaniaaltonen_db_user:${password}@ccluster0.efy2nbv.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
@@ -46,14 +25,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -63,35 +37,24 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-// *** TÄMÄ VIIMEISEKSI ***
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
